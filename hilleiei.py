b970e8b3e4b6ebb6ae96aac4d06018d0f67f6b42
chananya31 <52314868+chananya31@users.noreply.github.com>
2019-07-05 14:06:07 +0700

    Add files via upload
    โปรแกรมแรก

---
+++ hilleiei.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hill shop")
+print("---------------")
+print("1.water x 1 10THB = 10THB")
+print("---------------")
+print("2.lay x 3 10THB = 30THB ")
+print("---------------")
+print("3.icecream x 3 10THB = 30THB")
+print("------------------------------------------")
+print("Total              =70THB" )
+
+
+# In[2]:
+
+
+print(10)
+
+
+# In[5]:
+
+
+print(type(10))
+
+
+# In[7]:
+
+
+print(type("Hello"))
+
+
+# In[8]:
+
+
+print(type(True))
+
+
+# In[9]:
+
+
+print(type(10.3))
+
+
+# In[10]:
+
+
+print(type(1j))
+
+
+# In[12]:
+
+
+print(10+3.5)
+
+
+# In[ ]:
+
+
+
+
