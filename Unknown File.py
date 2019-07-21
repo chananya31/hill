497f97abfec338a62b6449eb1ef34ddf5b7a1d27
chananya31 <52314868+chananya31@users.noreply.github.com>
2019-07-21 23:05:41 +0700

    Add files via upload

---
+++ Unknown File.py
@@ -0,0 +1,26 @@
+print("----------menu----------")
+print("1.Strawberry 80 THB")
+print("2.Vanilla 50 THB")
+print("3.Chocalate 40 THB")
+print("4.Lemon 90 THB")
+print("5.Milk 10 THB")
+print("-------------------------")
+ch=int(input("Do you want"))
+
+if ch==1:
+ price=80
+ print("Your order:",ch,"price = ",price,"THB")
+elif ch==2:
+ price=50
+ print("Your order:",ch,"price = ",price,"THB")
+elif ch==3:
+ price=40
+ print("Your order:",ch,"price = ",price,"THB")
+elif ch==4:
+ prince=90
+ print("Your order:",ch,"price = ",price,"THB")
+elif ch==5:
+ prince=10
+ print("Your order:",ch,"price = ",price,"THB")
+else:
+ print("You not order: ")
